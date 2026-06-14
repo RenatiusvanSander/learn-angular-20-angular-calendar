b134ceed3e4d1269061c7f0c14b4b1ba9fb113d6
Remy Meier <remad@web.de>
2026-06-15 00:41:38 +0200

    feature/tutoring-appointment-calendar: static convert mezthods

--- src/app/project/models/tutoring-appointment.ts
+++ src/app/project/models/tutoring-appointment.ts
@@ -1,3 +1,5 @@
+import { AppointmentCalendarEventModel } from "./tutoring-appointment-calendar-event-model";
+
 export class TutoringAppointment {
     tutoringAppointmentNo!: number;
     tutoringAppointmentUser!: number; 
@@ -18,5 +20,18 @@ export class TutoringAppointment {
         newTutoringAppointment.isAccomplished = tutoringAppointment.isAccomplished;
 
         return newTutoringAppointment;
+    }
+
+    static fromAppointmentCalendarEventModel(calendarEvent: AppointmentCalendarEventModel) : TutoringAppointment {
+      const tutoringAppointment = new TutoringAppointment();
+      tutoringAppointment.tutoringAppointmentNo = calendarEvent.meta ? calendarEvent.meta.tutoringAppointmentNo as number : 0;
+      tutoringAppointment.tutoringAppointmentStartDateTime = calendarEvent.start.toISOString();
+      tutoringAppointment.tutoringAppointmentEndDateTime = calendarEvent.end ? calendarEvent.end.toISOString() : '';
+      tutoringAppointment.tutoringAppointmentDate = calendarEvent.start.toISOString().split('T')[0];
+      tutoringAppointment.isAccomplished = calendarEvent.meta ? calendarEvent.meta.isAccomplished : false;
+      tutoringAppointment.serviceContractId = calendarEvent.meta ? calendarEvent.meta.serviceContractId : 0;
+      tutoringAppointment.tutoringAppointmentUser = calendarEvent.meta ? calendarEvent.meta.tutoringAppointmentUser : 0;
+
+      return tutoringAppointment;
     }
 }
--- src/app/project/services/tutoring-appointment-mapper-service.ts
+++ src/app/project/services/tutoring-appointment-mapper-service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import { TutoringAppointment } from '../models/tutoring-appointment';
+import { AppointmentCalendarEventModel } from '../models/tutoring-appointment-calendar-event-model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TutoringAppointmentMapperService {
   
+  convertTutoringAppointmentToAppointmentCalendarEventModel(appointments: Array<TutoringAppointment>): Array<AppointmentCalendarEventModel> {
+    let calendarEvents: Array<AppointmentCalendarEventModel> = new Array<AppointmentCalendarEventModel>();
+    for (let appointment of appointments) {
+      let calendarEvent = AppointmentCalendarEventModel.fromTutoringAppointment(appointment);
+      calendarEvents.push(calendarEvent);
+    }
+
+    return calendarEvents;
+  }
+
+  convertAppointmentCalendarEventModelToTutoringAppointment(calendarEvents: Array<AppointmentCalendarEventModel>): Array<TutoringAppointment> {
+    let tutoringAppointments: Array<TutoringAppointment> = new Array<TutoringAppointment>();
+    for (let calendarEvent of calendarEvents) {
+      let tutoringAppointment = TutoringAppointment.fromAppointmentCalendarEventModel(calendarEvent);
+      tutoringAppointments.push(tutoringAppointment);
+    }
+
+    return tutoringAppointments;
+  }
 }
